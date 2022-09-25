faf70a850633fdf21905399ca89d43bd5a9c3062
wrx <1107269423@qq.com>
2022-09-25 16:58:11 +0800

    配置信息更改

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTVHL5xGOXWX4A1CKMh329MxAkdQ',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'PW4xXMOS6vp2J3EyC0EHEGwZAuEpeh2Yl_vJ2leZEBQ',
+      useTemplateId: '7umWj9KUDn6rvr7BElB3u6B4aK8rKM_qjVTLQkYQLC0',
       // 所在省份
       province: '北京',
       // 所在城市
@@ -212,7 +212,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'PW4xXMOS6vp2J3EyC0EHEGwZAuEpeh2Yl_vJ2leZEBQ',
+  CALLBACK_TEMPLATE_ID: '7umWj9KUDn6rvr7BElB3u6B4aK8rKM_qjVTLQkYQLC0',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
